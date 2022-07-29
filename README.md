284495442c20ac0cff888b1356513c116030d53c
Konstantin Demin <rockdrilla@gmail.com>
2022-07-29 23:03:54 +0300

    ufind-terse: optimize

--- README.md
+++ README.md
@@ -68,14 +68,7 @@ simple but slow (ref: [`ufind-terse.sh`](ufind-terse.sh))
 
 ```sh
 #!/bin/sh
-for i ; do
-    [ -n "$i" ] || continue
-    if [ -f "$i" ] ; then
-        printf '%s\0' "$i"
-    elif [ -d "$i" ] ; then
-        find "$i/" -follow -type f -print0
-    fi
-done \
+find "$@" -follow -type f -print0 \
 | xargs -0 -r -n 64 stat -L --printf='%d:%i|%n\0' \
 | sort -z -u -t '|' -k1,1 \
 | cut -z -d '|' -f 2
--- ufind-terse.sh
+++ ufind-terse.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 ## SPDX-License-Identifier: Apache-2.0
 ## (c) 2022, Konstantin Demin
-for i ; do
-    [ -n "$i" ] || continue
-    if [ -f "$i" ] ; then
-        printf '%s\0' "$i"
-    elif [ -d "$i" ] ; then
-        find "$i/" -follow -type f -print0
-    fi
-done \
+find "$@" -follow -type f -print0 \
 | xargs -0 -r -n 64 stat -L --printf='%d:%i|%n\0' \
 | sort -z -u -t '|' -k1,1 \
 | cut -z -d '|' -f 2
