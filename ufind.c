a08c7a5d2ffdb16b6c15ad6fa731267f8f6a21db
Konstantin Demin <rockdrilla@gmail.com>
2022-08-27 05:51:50 +0300

    release 0.2.2

--- ufind.c
+++ ufind.c
@@ -29,7 +29,7 @@
 static void usage(void)
 {
 	fprintf(stderr,
-		"ufind 0.2.1\n"
+		"ufind 0.2.2\n"
 		"Usage: ufind [-z] <path> [..<path>]\n"
 		"  -z  - separate entries with \\0 instead of \\n\n"
 	);
